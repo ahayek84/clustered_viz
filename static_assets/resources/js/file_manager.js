2a5fc54fc44d89ed0f0632d45a8b633c6656561a
Abdullrahman ElHayek <ahayek84@gmail.com>
2020-06-01 17:45:08 +0300

    sss push coreui assets2 33

--- static_assets/resources/js/file_manager.js
+++ static_assets/resources/js/file_manager.js
@@ -1,10 +1,12 @@
 // module "file_manager.js"
 import Constants from './constants.js';
+
 export default class FileManager {
     constructor() {
         this.file = '';
         this.con = new Constants()
     }
+
     /// it has to be added as directive
     function
 
@@ -14,30 +16,48 @@ export default class FileManager {
     }
 
     getKeyByValue(object, value) {
-      return Object.keys(object).find(key => object[key] === value);
+        return Object.keys(object).find(key => object[key] === value);
     }
 
     // Adding a method to the constructor
     get_geo_names(codes, type = 'en') {
         var dic = this.con.get_geoname_lookup(type)
         var newcodes = codes.map(x => dic[x])
-        console.log(newcodes)
         return newcodes
     }
 
-    get_style_map(type = 'en'){
+    // Adding a method to the constructor
+    get_local_names(codes, type = 'en') {
+        var dic = this.con.get_localname_lookup(type)
+        var newcodes = codes.map(x => dic[x])
+        return newcodes
+    }
+
+    get_style_map(type = 'en') {
         var res = "mapbox://styles/pmaoque/cjvnmbu4v03c51dp9ztq1vfnw"
-        if (type == 'ar'){
+        if (type == 'ar') {
             res = "mapbox://styles/pmaoque/cjvnm9c5w01o71cptm2xe655j"
         }
         return res
     }
 
-    get_geojson_map(type = 'en'){
+    get_geojson_map(type = 'en') {
         var li = this.con.get_list_geojson()
         var res = li.governorates_en
-        if (type == 'ar'){
+        if (type == 'ar') {
             res = li.governorates_ar
+        }
+        return res
+    }
+
+    get_geojson_locality_map(type = 'en', id) {
+        var li = this.con.get_list_geojson()
+        var res = ''
+        if (id == 1345) {
+                res = li.bethlehem_localities
+            if (type == 'ar') {
+                res = li.bethlehem_localities_ar
+            }
         }
         return res
     }
@@ -59,16 +79,34 @@ export default class FileManager {
 
     getFileJSON(file) {
         var text = this.readTextFile(this.getURL() + "static/resources/geojson/" + file)
-        var data = JSON.parse(text);
+        var data = ''
+        if (text == null) {
+            data = {features: []}
+        } else {
+            data = JSON.parse(text);
+        }
         return data
     }
 
-    getFeatures(js){
+    getFeatures(js) {
         return js['features']
     }
 
     getURL() {
         return window.location.href;
+    }
+
+    extract_dictt(file, id_fet, fet2) {
+        var x = this.getFeatures(this.getFileJSON(file))
+        var dictt = {}
+        var i;
+        for (i = 0; i < x.length; i++) {
+            dictt[x[i]['properties'][id_fet]] = x[i]['properties'][fet2];
+        }
+        return dictt
+        // usage
+        // var x = fm.extract_dictt(li.bethlehem_localities,'ID','NAMEAR')
+        // console.log(x)
     }
 }
 
---
+++ static_assets/resources/js/map_manager.js
@@ -0,0 +1,73 @@
+import Constants from "./constants.js";
+import FileManager from './file_manager.js';
+
+export default class MapManager {
+    constructor() {
+        this.file = '';
+        this.con = new Constants()
+        this.fm = new FileManager()
+        this.li = this.con.get_list_geojson()
+        this.geo_id = ["1235", "2475", "2455", "1110", "1240", "2460", "2470", "1120",
+            "1101", "2465", "1125", "1350", "1230", "1105", "1115", "1345"]
+        this.local_id = [
+            13452170, 13452175, 13452180,13452185,13452190,13452195,13452200,13452360,13452660
+        ]
+    }
+
+    /// it has to be added as directive
+    function
+
+    // Adding a method to the constructor Dont Delete it
+    greet() {
+        console.log('haha')
+    }
+
+    plot_map() {
+        var lang = "ar"
+        var geo_name_en = this.fm.get_geo_names(this.geo_id, lang)
+        var geo_json_map = this.fm.get_geojson_map(lang)
+        var geo_json_loc_map = this.fm.get_geojson_locality_map(lang,1345) // betheleme
+        var data = [{
+            type: "choroplethmapbox",
+            name: "Palestine",
+            geojson: this.fm.getFileJSON(geo_json_map),
+            locations: geo_name_en,
+            z: [141, 140, 155, 147, 132, 146, 151, 137, 146,
+                136, 145, 141, 149, 151, 138, 164],
+            zmin: 25,
+            zmax: 280,
+            showlegend:true,
+            showscale:false,
+            colorbar: {y: 0, yanchor: "bottom", title: {text: "Palestine", side: "right"}}
+        },
+            {
+                type: "choroplethmapbox",
+                autocolorscale: false,
+                colorscale:[[0, 'rgb(0,0,255)'], [1, 'rgb(255,0,0)']],
+                name: "Bethlehem",
+                geojson: this.fm.getFileJSON(geo_json_loc_map),
+                locations: this.fm.get_local_names(this.local_id, lang),
+                z: [141, 140, 155, 147, 132, 146, 151, 100, 50],
+                zmin: 25,
+                zmax: 280,
+                showlegend:true,
+                showscale:false,
+                colorbar: {y: 0, yanchor: "bottom", title: {text: "Palestine", side: "right"}}
+            }
+        ];
+
+        var layout = {
+            mapbox: {style: this.fm.get_style_map(lang), center: {lon: 35.208116, lat: 31.814685}, zoom: 7.5},
+            // width: 600,
+            //height: 500,
+            margin: {t: 0, b: 0}
+        };
+
+        var options = {
+            mapboxAccessToken: 'pk.eyJ1IjoicG1hb3F1ZSIsImEiOiJjanZpMHNnNGowMHlvM3lxbHA3ZWdodzlyIn0.6m3CU9tWBXh1Jt7xMRYxOw',
+            responsive: true
+        }
+
+        Plotly.newPlot("myDiv", data, layout, options);
+    }
+}
