f6dc62f45ab0dcca271b5112fb2f2b976a69c422
Abdullrahman ElHayek <ahayek84@gmail.com>
2020-06-02 14:37:28 +0300

    put control over map refresh

--- static_assets/resources/js/home/map.js
+++ static_assets/resources/js/home/map.js
@@ -1,3 +1,8 @@
 import MapManager from './../map_manager.js';
 var map = new MapManager()
-map.plot_map()
+map.init_map()
+
+// In order to call function from module javascript Make it global
+function refresh_map() {map.refresh_map()}
+window.refresh_map = refresh_map;
+// The Line above makes it global
