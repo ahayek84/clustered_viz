bd5ec260419a9fda30da825804b270e5a803c845
Abdullrahman ElHayek <ahayek84@gmail.com>
2020-07-24 21:04:10 +0300

    sunburst v5

--- static_assets/resources/js/sunburst/sunburst.js
+++ static_assets/resources/js/sunburst/sunburst.js
@@ -1,44 +1,35 @@
 var oldWidth = 0
+var svg = 0
+let node
+var x = 0
+var y = 0
+var colors = 0
+var partition = 0
+var arc = 0
+var radius = 0
+
 ///////
-
-
 function getURL() {
     return window.location.origin;
 }
 
 ////////
-function render() {
-    if (oldWidth == innerWidth) return
-    oldWidth = innerWidth
-    var width = d3.select('#graph').node().offsetWidth
-    var height = width
-    if (innerWidth <= 925) {
-        width = innerWidth
-        height = innerHeight * .7
-    }
-    var radius = Math.min(width, height) / 2;
-    let node
-
-
-    const svg = d3.select('#graph')
-        .append('svg')
-        .attr('width', width)
-        .attr('height', height)
-        .append('g')
-        .attr('transform', 'translate(' + width / 2 + ',' + (height / 2) + ')')
-
-    const x = d3.scaleLinear().range([0, 2 * Math.PI])
-    const y = d3.scaleLinear().range([0, radius])
-
-    const colors = d3.scaleOrdinal(d3.schemeCategory20)
-    const partition = d3.partition()
-
-    const arc = d3.arc()
+function setSvg(svgn, radiusn) {
+    svg = svgn
+    x = d3.scaleLinear().range([0, 2 * Math.PI])
+    radius = radiusn
+    y = d3.scaleLinear().range([0, radius])
+    colors = d3.scaleOrdinal(d3.schemeCategory20)
+    partition = d3.partition()
+    arc = d3.arc()
         .startAngle(d => Math.max(0, Math.min(2 * Math.PI, x(d.x0))))
         .endAngle(d => Math.max(0, Math.min(2 * Math.PI, x(d.x1))))
         .innerRadius(d => Math.max(0, y(d.y0)))
         .outerRadius(d => Math.max(0, y(d.y1)))
+}
 
+///////
+function sunburst() {
     d3.json(getURL() + "/static/resources/js/" + "flare.json", function (error, root) {
         if (error) return console.error(error)
 
@@ -53,7 +44,7 @@ function render() {
                 if (d.depth === 0) return 'white'
                 return colors(d.x0)
             })
-            .on('click', click)
+            .on('click', update)
 
         gSlices.append('text')
             .attr('dy', '.35em')
@@ -89,105 +80,107 @@ function render() {
 
     })
 
-    function arcTweenText(a, i) {
-        var oi = d3.interpolate({
-            x0: (a.x0s ? a.x0s : 0),
-            x1: (a.x1s ? a.x1s : 0),
-            y0: (a.y0s ? a.y0s : 0),
-            y1: (a.y1s ? a.y1s : 0)
-        }, a)
+}
 
-        function tween(t) {
-            var b = oi(t)
-            var ang = ((x((b.x0 + b.x1) / 2) - Math.PI / 2) / Math.PI * 180)
+/////
+function arcTweenText(a, i) {
+    var oi = d3.interpolate({
+        x0: (a.x0s ? a.x0s : 0),
+        x1: (a.x1s ? a.x1s : 0),
+        y0: (a.y0s ? a.y0s : 0),
+        y1: (a.y1s ? a.y1s : 0)
+    }, a)
 
-            b.textAngle = (ang > 90) ? 180 + ang : ang
-            a.centroid = arc.centroid(b)
+    function tween(t) {
+        var b = oi(t)
+        var ang = ((x((b.x0 + b.x1) / 2) - Math.PI / 2) / Math.PI * 180)
 
-            return 'translate(' + arc.centroid(b) + ')rotate(' + b.textAngle + ')'
+        b.textAngle = (ang > 90) ? 180 + ang : ang
+        a.centroid = arc.centroid(b)
+
+        return 'translate(' + arc.centroid(b) + ')rotate(' + b.textAngle + ')'
+    }
+
+    return tween
+}
+
+///
+function arcTweenPath(a, i) {
+    var oi = d3.interpolate({
+        x0: (a.x0s ? a.x0s : 0),
+        x1: (a.x1s ? a.x1s : 0),
+        y0: (a.y0s ? a.y0s : 0),
+        y1: (a.y1s ? a.y1s : 0)
+    }, a)
+
+    function tween(t) {
+        var b = oi(t)
+
+        a.x0s = b.x0
+        a.x1s = b.x1
+        a.y0s = b.y0
+        a.y1s = b.y1
+
+        return arc(b)
+    }
+
+    if (i == 0 && node) {
+
+        var xd = d3.interpolate(x.domain(), [node.x0, node.x1])
+        var yd = d3.interpolate(y.domain(), [node.y0, 1])
+        var yr = d3.interpolate(y.range(), [node.y0 ? 40 : 0, radius])
+
+        return function (t) {
+            x.domain(xd(t))
+            y.domain(yd(t)).range(yr(t))
+
+            return tween(t)
         }
-
+    } else {
+        // first build
         return tween
     }
+}
 
-    function arcTweenPath(a, i) {
-        var oi = d3.interpolate({
-            x0: (a.x0s ? a.x0s : 0),
-            x1: (a.x1s ? a.x1s : 0),
-            y0: (a.y0s ? a.y0s : 0),
-            y1: (a.y1s ? a.y1s : 0)
-        }, a)
+//
+function update(d) {
+    node = d
+    const total = d.x1 - d.x0
 
-        function tween(t) {
-            var b = oi(t)
+    svg.selectAll('path')
+        .transition('click')
+        .duration(750)
+        .attrTween('d', function (d, i) {
+            return arcTweenPath(d, i)
+        })
 
-            a.x0s = b.x0
-            a.x1s = b.x1
-            a.y0s = b.y0
-            a.y1s = b.y1
-
-            return arc(b)
-        }
-
-        if (i == 0 && node) {
-
-            var xd = d3.interpolate(x.domain(), [node.x0, node.x1])
-            var yd = d3.interpolate(y.domain(), [node.y0, 1])
-            var yr = d3.interpolate(y.range(), [node.y0 ? 40 : 0, radius])
-
-            return function (t) {
-                x.domain(xd(t))
-                y.domain(yd(t)).range(yr(t))
-
-                return tween(t)
+    svg.selectAll('text')
+        .transition('click')
+        .attr('opacity', 1)
+        .duration(750)
+        .attrTween('transform', function (d, i) {
+            return arcTweenText(d)
+        })
+        .attr('text-anchor', function (d) {
+            return d.textAngle > 180 ? 'start' : 'end'
+        })
+        .attr('dx', function (d) {
+            if (d.data.name.length > 3) {
+                return d.textAngle > 180 ? -23 : 23
             }
-        } else {
-            // first build
-            return tween
-        }
-    }
-
-    function click(d) {
-        node = d
-        const total = d.x1 - d.x0
-
-        svg.selectAll('path')
-            .transition('click')
-            .duration(750)
-            .attrTween('d', function (d, i) {
-                return arcTweenPath(d, i)
-            })
-
-        svg.selectAll('text')
-            .transition('click')
-            .attr('opacity', 1)
-            .duration(750)
-            .attrTween('transform', function (d, i) {
-                return arcTweenText(d)
-            })
-            .attr('text-anchor', function (d) {
-                return d.textAngle > 180 ? 'start' : 'end'
-            })
-            .attr('dx', function (d) {
-                if (d.data.name.length > 3) {
-                    return d.textAngle > 180 ? -23 : 23
-                }
-                return d.textAngle > 180 ? -13 : 13
-            })
-            .attr('opacity', function (e) {
-                // hide & show text
-                if (e.x0 >= d.x0 && e.x1 <= (d.x1 + 0.0000000000000001)) {
-                    const arcText = d3.select(this.parentNode).select('text')
-                    arcText.attr('visibility', function (d) {
-                        return (d.x1 - d.x0) / total < 0.01 ? 'hidden' : 'visible'
-                    })
-                } else {
-                    return 0
-                }
-            })
-    }
-
+            return d.textAngle > 180 ? -13 : 13
+        })
+        .attr('opacity', function (e) {
+            // hide & show text
+            if (e.x0 >= d.x0 && e.x1 <= (d.x1 + 0.0000000000000001)) {
+                const arcText = d3.select(this.parentNode).select('text')
+                arcText.attr('visibility', function (d) {
+                    return (d.x1 - d.x0) / total < 0.01 ? 'hidden' : 'visible'
+                })
+            } else {
+                return 0
+            }
+        })
 }
 
 
-render()
